dca7995ae53b44ed1c290473156820defae8f138
Nick Coad <dev@coader.io>
2019-08-29 14:06:24 +1000

    chore: fixed examples

---
+++ 2.2/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace restore_example
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
